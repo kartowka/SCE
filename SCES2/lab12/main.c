ac756f4e20edbde05ef5fb2afef6741fef89a861
Anthony Fleysher <antonfley@gmail.com>
2019-05-20 16:15:48 +0300

    added exercise 4

--- SCES2/lab12/main.c
+++ SCES2/lab12/main.c
@@ -71,143 +71,229 @@
 //}
 
 ////EXERCISE2//
-#define SIZE 30
+//#define SIZE 30
+//typedef struct {
+//	char *bookName;
+//	char *name;
+//	int numOfPages;
+//	int numOfPhotos;
+//	int year;
+//}Book;
+//
+//Book* alloc1D(int *);
+//void getBook(Book*, int);
+//void freeBook(Book**, int);
+//void sortBookByYear(Book**, int);
+//void printBook(Book*,int);
+//void swap(Book*, Book*);
+//void printBookByPages(Book*, int);
+//void addRemoveBook(Book**, int*);
+//
+//	int main() {
+//	Book *book = NULL;
+//	int size,i;
+//	book = alloc1D(&size);
+//	for (i = 0; i < size; ++i) {
+//		getBook(&book[i], i);
+//	}
+//	printBookByPages(book, size);
+//	addRemoveBook(&book, &size);
+//	sortBookByYear(&book, size);
+//	for (i = 0; i < size; ++i) {
+//		printBook(book + i, i);
+//	}
+//	freeBook(&book, size);
+//	free(book);
+//	return 0;
+//}
+//Book* alloc1D(int *size) {
+//	Book *pBook = NULL;
+//	printf("enter number of books: ");
+//	scanf("%d", size);
+//	pBook = (Book*)malloc(*size * sizeof(Book));
+//	if (!pBook)
+//		puts("error allocate struct");
+//	return pBook;
+//}
+//void getBook(Book* pb, int index) {
+//	printf("enter details for %d book\n", index + 1);
+//	getchar();
+//	char buffer[SIZE];
+//	printf("enter book name: ");
+//	scanf("%[^\n]", buffer);
+//	getchar();
+//	pb->bookName = (char*)malloc(strlen(buffer) + 1);
+//	strcpy(pb->bookName, buffer);
+//	printf("enter book Author: ");
+//	scanf("%[^\n]", buffer);
+//	getchar();
+//	pb->name = (char*)malloc(strlen(buffer) + 1);
+//	strcpy(pb->name, buffer);
+//	printf("enter number of pages: ");
+//	scanf("%d", &(pb->numOfPages));
+//	printf("enter number of photos: ");
+//	scanf("%d", &(pb->numOfPhotos));
+//	printf("enter year: ");
+//	scanf("%d", &(pb->year));
+//}
+//void printBookByPages(Book* b, int size) {
+//	int i,max=b[0].numOfPages,index=0;
+//	for (i = 1; i < size; ++i) {
+//		if (b[i].numOfPages > max) {
+//			max = b[i].numOfPages;
+//			index = i;
+//		}
+//	}
+//	printBook(b+index, index);
+//}
+//void freeBook(Book **pb, int size) {
+//	int i;
+//	for (i = 0; i < size; ++i) {
+//		free((*pb+i)->bookName);
+//		free((*pb+i)->name);
+//	}
+//}
+//
+//void sortBookByYear(Book** pb, int size) {
+//	int i,j;
+//	for (i = 0; i < size; ++i) {
+//		for (j = 0; j < (size-i-1); ++j) {
+//			if ((*pb + j)->year > ((*pb + j + 1)->year)) {
+//				swap(&(*pb)[j], &(*pb)[j + 1]);
+//			}
+//		}
+//	}
+//}
+//void printBook(Book* b,int index) {
+//	printf("Details for Book %d :\n", index + 1);
+//	printf("Book name: %s\n", b->bookName);
+//	printf("Book Author: %s\n", b->name);
+//	printf("Pages: %d\n", b->numOfPages);
+//	printf("Photos: %d\n", b->numOfPhotos);
+//	printf("Year: %d\n", b->year);
+//}
+//
+//void swap(Book* A, Book* B) {
+//	Book temp = *A;
+//	*A = *B;
+//	*B = temp;
+//}
+//void addRemoveBook(Book** pb, int* pSize) {
+//	int choice, rBook, i;
+//	for (;;) {
+//		do {
+//			printf("what you would like to do?\n");
+//			printf("enter 1 for add Book\n");
+//			printf("enter 2 for delete Book\n");
+//			printf("enter 0 for exit\n");
+//			scanf("%d", &choice);
+//		} while (choice > 2 || choice < 0);
+//		if (choice == 1) {
+//			(*pSize)++;
+//			*pb = (Book*)realloc(*pb, *pSize * sizeof(Book));
+//			getBook(&(*pb)[*pSize - 1], *pSize - 1);
+//		}
+//		else if (choice == 2) {
+//			do {
+//				printf("which book you would like to remove?\n");
+//				scanf("%d", &rBook);
+//				rBook--;
+//			} while (rBook > *pSize - 1 || rBook < 0);
+//			for (i = rBook; i < *pSize-1; ++i) {
+//				(*pb)[i] = (*pb)[i + 1]; //swap to disired location.
+//			}
+//			(*pSize)--;
+//			free((*pb)[*pSize].bookName);
+//			free((*pb)[*pSize].name);
+//			*pb = (Book*)realloc(*pb, *pSize * sizeof(Book));
+//		}
+//		else
+//			break;
+//	}
+//}
+
+////EXERCISE3//
+#define SIZE 10
+#define BUFFER 128
+typedef enum{FEMALE,MALE}Sex;
 typedef struct {
-	char *bookName;
-	char *name;
-	int numOfPages;
-	int numOfPhotos;
+	int day;
+	int month;
 	int year;
-}Book;
+}Birthday;
+typedef struct {
+	char* name;
+	char* surname;
+	Sex gender;
+	Birthday list;
+	char id[SIZE];
+}Person;
 
-Book* alloc1D(int *);
-void getBook(Book*, int);
-void freeBook(Book**, int);
-void sortBookByYear(Book**, int);
-void printBook(Book*,int);
-void swap(Book*, Book*);
-void printBookByPages(Book*, int);
-void addRemoveBook(Book**, int*);
 
-	int main() {
-	Book *book = NULL;
-	int size,i;
-	book = alloc1D(&size);
+
+void getPerson(Person*,int);
+void printPerson(Person* , int);
+Person* alloc1D(int*);
+
+int main() {
+	Person *pPerson = NULL;
+	int size, i;
+	pPerson = alloc1D(&size);
 	for (i = 0; i < size; ++i) {
-		getBook(&book[i], i);
+		getPerson(&pPerson[i], i);
 	}
-	printBookByPages(book, size);
-	addRemoveBook(&book, &size);
-	sortBookByYear(&book, size);
 	for (i = 0; i < size; ++i) {
-		printBook(book + i, i);
+		printPerson(pPerson+i, i);
 	}
-	freeBook(&book, size);
-	free(book);
+	free(pPerson);
 	return 0;
 }
-Book* alloc1D(int *size) {
-	Book *pBook = NULL;
-	printf("enter number of books: ");
+Person* alloc1D(int *size) {
+	Person *pPerson = NULL;
+	printf("enter number of Persons: ");
 	scanf("%d", size);
-	pBook = (Book*)malloc(*size * sizeof(Book));
-	if (!pBook)
+	pPerson = (Person*)malloc(*size * sizeof(Person));
+	if (!pPerson)
 		puts("error allocate struct");
-	return pBook;
+	return pPerson;
 }
-void getBook(Book* pb, int index) {
-	printf("enter details for %d book\n", index + 1);
+void getPerson(Person* p, int index) {
+	printf("enter details for %d Person\n", index + 1);
 	getchar();
-	char buffer[SIZE];
-	printf("enter book name: ");
+	char buffer[BUFFER];
+	printf("enter Person name: ");
 	scanf("%[^\n]", buffer);
 	getchar();
-	pb->bookName = (char*)malloc(strlen(buffer) + 1);
-	strcpy(pb->bookName, buffer);
-	printf("enter book Author: ");
+	p->name = (char*)malloc(strlen(buffer) + 1);
+	strcpy(p->name, buffer);
+	printf("enter Person surname : ");
 	scanf("%[^\n]", buffer);
 	getchar();
-	pb->name = (char*)malloc(strlen(buffer) + 1);
-	strcpy(pb->name, buffer);
-	printf("enter number of pages: ");
-	scanf("%d", &(pb->numOfPages));
-	printf("enter number of photos: ");
-	scanf("%d", &(pb->numOfPhotos));
-	printf("enter year: ");
-	scanf("%d", &(pb->year));
-}
-void printBookByPages(Book* b, int size) {
-	int i,max=b[0].numOfPages,index=0;
-	for (i = 1; i < size; ++i) {
-		if (b[i].numOfPages > max) {
-			max = b[i].numOfPages;
-			index = i;
-		}
-	}
-	printBook(b+index, index);
-}
-void freeBook(Book **pb, int size) {
-	int i;
-	for (i = 0; i < size; ++i) {
-		free((*pb+i)->bookName);
-		free((*pb+i)->name);
-	}
+	p->surname = (char*)malloc(strlen(buffer) + 1);
+	strcpy(p->surname, buffer);
+	printf("enter ID: ");
+	scanf("%[^\n]", p->id);
+	getchar();
+	do {
+		printf("enter Gender 1 for male 0 for female\n");
+		scanf("%d", &p->gender);
+	} while (p->gender <0 || p->gender > 1);
+	printf("enter Birthday: day,month,year\n");
+	scanf("%d", &p->list.day);
+	scanf("%d", &p->list.month);
+	scanf("%d", &p->list.year);
 }
 
-void sortBookByYear(Book** pb, int size) {
-	int i,j;
-	for (i = 0; i < size; ++i) {
-		for (j = 0; j < (size-i-1); ++j) {
-			if ((*pb + j)->year > ((*pb + j + 1)->year)) {
-				swap(&(*pb)[j], &(*pb)[j + 1]);
-			}
-		}
-	}
-}
-void printBook(Book* b,int index) {
-	printf("Details for Book %d :\n", index + 1);
-	printf("Book name: %s\n", b->bookName);
-	printf("Book Author: %s\n", b->name);
-	printf("Pages: %d\n", b->numOfPages);
-	printf("Photos: %d\n", b->numOfPhotos);
-	printf("Year: %d\n", b->year);
-}
-
-void swap(Book* A, Book* B) {
-	Book temp = *A;
-	*A = *B;
-	*B = temp;
-}
-void addRemoveBook(Book** pb, int* pSize) {
-	int choice, rBook, i;
-	for (;;) {
-		do {
-			printf("what you would like to do?\n");
-			printf("enter 1 for add Book\n");
-			printf("enter 2 for delete Book\n");
-			printf("enter 0 for exit\n");
-			scanf("%d", &choice);
-		} while (choice > 2 || choice < 0);
-		if (choice == 1) {
-			(*pSize)++;
-			*pb = (Book*)realloc(*pb, *pSize * sizeof(Book));
-			getBook(&(*pb)[*pSize - 1], *pSize - 1);
-		}
-		else if (choice == 2) {
-			do {
-				printf("which book you would like to remove?\n");
-				scanf("%d", &rBook);
-				rBook--;
-			} while (rBook > *pSize - 1 || rBook < 0);
-			for (i = rBook; i < *pSize-1; ++i) {
-				(*pb)[i] = (*pb)[i + 1]; //swap to disired location.
-			}
-			(*pSize)--;
-			free((*pb)[*pSize].bookName);
-			free((*pb)[*pSize].name);
-			*pb = (Book*)realloc(*pb, *pSize * sizeof(Book));
-		}
-		else
-			break;
-	}
+void printPerson(Person* p, int index) {
+	printf("Details for Person %d :\n", index + 1);
+	printf("Person name: %s\n", p->name);
+	printf("Person surname: %s\n", p->surname);
+	printf("Person ID: %s\n", p->id);
+	printf("Person Birthday %d/%d/%d\n", p->list.day, p->list.month, p->list.year);
+	printf("Person Gender: ");
+	if (p->gender == 0)
+		printf("Female");
+	else
+		printf("Male");
+	printf("\n");
 }
